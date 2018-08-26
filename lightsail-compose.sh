d4d38553e115d8bc35218e8a5434cc5c43bb3535
Mark <mark.holthaus@gmail.com>
2018-08-26 10:39:28 -0700

    Update lightsail-compose.sh

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -4,22 +4,22 @@
 curl -sSL https://get.docker.com | sh
 
 # make it so you don't need to sudo to run docker commands
-usermod -aG docker ubuntu
+sudo usermod -aG docker ubuntu
 
 # install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # copy the dockerfile into /srv/docker
 # if you change this, change the systemd service file to match
 # WorkingDirectory=[whatever you have below]
-mkdir /srv/docker
-curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/Momojunzi/new-portfolio/master/docker-compose.yaml
+sudo mkdir /srv/docker
+sudo curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/Momojunzi/new-portfolio/master/docker-compose.yaml
 
 # copy in systemd unit file and register it so our compose file runs
 # on system restart
-curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/Momojunzi/aws-lightsail-docker-launch-script/master/docker-compose-app.service
+sudo curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/Momojunzi/aws-lightsail-docker-launch-script/master/docker-compose-app.service
 systemctl enable docker-compose-app
 
 # start up the application via docker-compose
-docker-compose -f /srv/docker/docker-compose.yml up -d
+sudo docker-compose -f /srv/docker/docker-compose.yml up -d
